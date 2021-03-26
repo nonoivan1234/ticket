c7fcb84f6e01a3bd409a0eff54d17b5150881b62
nonoivan1234 <nonoivan0627@gmail.com>
2021-03-26 23:33:01 +0800

    header
    try to use header to post

--- test_select_seat.py
+++ test_select_seat.py
@@ -2,14 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-url = 'https://ticket.com.tw/Application/UTK02/UTK0202_.aspx?PERFORMANCE_ID=N0VTGV3K&PRODUCT_ID=N0VQ76BZ#'
+url = 'https://ticket.com.tw/Application/UTK02/UTK0202_.aspx?PERFORMANCE_ID=N0VTGVMM&PRODUCT_ID=N0VQ76BZ'
 
-r = requests.get(url)
+headers = {
+    'access-control-allow-credentials': 'true',
+    'access-control-allow-origin': 'https://ticket.com.tw',
+    'alt-svc': 'h3-29=":443"; ma=3600,h3-27=":443"; ma=3600',
+    'content-length': '0',
+    'content-type': 'text/plain',
+    'cross-origin-resource-policy': 'cross-origin',
+    'date': 'Fri, 26 Mar 2021 14:41:29 GMT',
+    'priority': 'u=3,i',
+    'server': 'proxygen-bolt',
+    'strict-transport-security': 'max-age=31536000; includeSubDomains'
+}
+r = requests.post(url,headers = headers)
 soup = BeautifulSoup(r.text,'lxml')
 pic = soup.find('img',id = 'chk_pic')
-
+print(r.text)
 link = 'https://ticket.com.tw/'+pic.get("src")
-print(link)
+
 
 if not os.path.exists('chk_pic'):
     os.makedirs('chk_pic')
